e8b60868fdb284702b8ace86efd6f5a12a9439cb
vuce <info@kombi-nat.de>
2018-10-29 21:54:05 +0100

    init

---
+++ app/src/main/java/ba/kombi/com/ba/MainActivity.kt
@@ -0,0 +1,12 @@
+package ba.kombi.com.ba
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
